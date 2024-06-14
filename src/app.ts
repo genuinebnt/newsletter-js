51d1b825e91190b3003601ce61237c46b8a3a245
Genuine Basil N T <genuine.basilnt@gmail.com>
2024-06-14 10:46:06 +0530

    added axios and spawnApp

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-import express, { Express } from "express";
+import express, { Application } from "express";
 import dotenv from "dotenv";
 
 import healthCheckRoute from "./routes/health-check";
 
 dotenv.config();
 
-const app: Express = express();
+const app: Application = express();
 
 app.use("/healthCheck", healthCheckRoute);
 
--- tests/health-check.test.ts
+++ tests/health-check.test.ts
@@ -1,16 +1,64 @@
-import request from "supertest";
-
 import app from "../src/app";
+import { Server } from "http";
+import axios from "axios";
+
+const spawnApp = async (): Promise<Server> => {
+  return new Promise((resolve, reject) => {
+    const server = app.listen(8000, () => {
+      console.log(`Server starting on port 8000`);
+      resolve(server);
+    });
+
+    server.on("error", (err) => {
+      console.error(`Failed to start server on port 8000: ${err}`);
+      reject(err);
+    });
+  });
+};
 
 describe("health check", () => {
+  let server: Server;
+
+  beforeAll(async () => {
+    server = await spawnApp();
+  });
+
+  afterAll(async () => {
+    server.close();
+  });
+
   test("health check works", async () => {
-    const res = await request(app).get("/healthCheck");
-    expect(res.statusCode).toBe(200);
+    await axios
+      .get("http://localhost:8000/healthCheck")
+      .then((response) => expect(response.status).toBe(200));
   });
 
   test("subscribe returns 200 for valid form data", async () => {
-    let body = "name=genuine&email=genuine.basilnt%40gmail.com"
-    const res = await request(app).post("/subscriptions").set("Content-Type", "applicaton/x-www-form-urlencoded");
-    expect(res.statusCode).toBe(200);
-  })
+    let body = "name=genuine&email=genuine.basilnt%40gmail.com";
+    await axios
+      .post("http://localhost:8000/subscriptions", body, {
+        headers: {
+          "Content-Type": "applicaton/x-www-form-urlencoded",
+        },
+      })
+      .then((response) => expect(response.status).toBe(200));
+  });
+
+  test("subscribe returns 400 when data is missing", async () => {
+    let testCases = [
+      { body: "name=genuine", errorMessage: "missing email" },
+      { body: "email=genuine.basilnt@gmail.com", errorMessage: "missing name" },
+      { body: "", errorMessage: "missing name and email" },
+    ];
+
+    for (const { body, errorMessage } of testCases) {
+      await axios
+        .post("http://localhost:8000/subscriptions", body, {
+          headers: {
+            "Content-Type": "applicaton/x-www-form-urlencoded",
+          },
+        })
+        .then((response) => expect(response.statusText).toBe(errorMessage));
+    }
+  });
 });
